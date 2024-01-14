52594e9c438fd1e3aa53d96e6a82c62e50dd3a7d
Giov009 <giovanny.cuaical117@gmail.com>
2024-01-13 19:04:58 -0500

    cammbio_11

--- apl_main.py
+++ apl_main.py
@@ -1,3 +1,5 @@
+#este es un ejemplo de cambio
+
 
 # Importación del módulo logging, que proporciona funcionalidades de registro para mostrar mensajes informativos,
 # de advertencia o de error durante la ejecución del script. Ayuda a rastrear la actividad del programa.
