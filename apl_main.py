6bc7b15f2aba0fa82b5467e6cb1d1ca164ea138c
Yil2190 <yilmaespitia@gmail.com>
2024-01-18 22:24:38 -0500

    Quitar comentarios
    Se ajustaron los comentarios usando docstrings .

--- apl_main.py
+++ apl_main.py
@@ -1,41 +1,34 @@
-#este es un ejemplo de cambio
-#ejemplo 2 para cambios
-# prueba_1 para cambios en el código principal
-# prueba_2 para cambios en el código principal
-# prueba_3 juanjose para cambios en el codigo principal
-
-
-
-# prueba completa de rama
-
-
-# Importación del módulo logging, que proporciona funcionalidades de registro para mostrar mensajes informativos,
-# de advertencia o de error durante la ejecución del script. Ayuda a rastrear la actividad del programa.
 import logging
 
-# Importación del módulo os, que ofrece funciones para interactuar con el sistema operativo, en este caso, para crear carpetas.
 import os
 
-# Importación del módulo random, que permite generar valores aleatorios. Se utiliza para asignar aleatoriamente missions y number_filess.
 import random
 
-# Importación de las clases datetime y timedelta desde el módulo datetime.
-# Esto se hace para trabajar con fechas y tiempos, en este caso, para obtener la fecha actual.
 from datetime import datetime, timedelta
 
-# Importación del módulo time, que proporciona funciones relacionadas con el tiempo.
-# Se utiliza específicamente la función sleep para introducir pausas en la ejecución del script, en este caso, para esperar 2 minutos.
 import time
 
-# juanjo 
-
 def generate_file_names():
+    """ 
+    Generates file names for space missions.
+    Returns a string with the file name APL{idd}-0000{number_files}.log
+    The mission is selected randomly
+    """
     missions = ["OrbitOne", "ColonyMoon", "VacMars", "GalaxyTwo"]
     idd = random.choice(missions)
     number_files = random.randint(1, 1000)
     return f"APL{idd}-0000{number_files}.log"
 
+
 def generate_content_files(idd):
+    """
+    Generates content for device state files.
+    Args idd: string as device identifier
+    Returns str: a string containing information about the state of the device,
+    including the current date,device identifier (IDD) and device status.
+    The status is randomly chosen 
+    from the list states["excellent", "good", "warning", "faulty", "killed", "unknown"]
+    """
     states = ["excellent", "good", "warning", "faulty", "killed", "unknown"]
     current_date = datetime.now().strftime("%d%m%y%H%M%S")
     device_status = random.choice(states)
@@ -44,7 +37,15 @@ def generate_content_files(idd):
                 Estado del dispositivo: {device_status}"""
     return content
 
+
 def Synchronization():
+    """
+    This function create a folder named 'devices' in the current director
+    If the folder is created successfully, it logs an information message.
+    If the folder already exists, it logs a warning message.
+    If any other exception occurs during the folder creation
+    it logs a warning message with the error details.
+    """
     folder_name = "devices"
     try:
         os.mkdir(folder_name)
@@ -53,13 +54,20 @@ def Synchronization():
         logging.warning("La carpeta {} ya existe".format(folder_name))
     except Exception as e:
         logging.warning("Error al crear la carpeta: {}".format(e))
+
         
 def generate():
+    """
+    Generates files and folders in the 'devices' directory.
+    This function creates a subfolder with a name based on the current date and time
+    inside the 'devices' directory. It then generates between 1 and 10 files every 2 minutes,
+    each containing information about the device status.
+    """
     folder_name = "devices"
     current_date = datetime.now().strftime("%d%m%y%H%M%S")
     subfolder_name = os.path.join(folder_name, current_date)
     os.mkdir(subfolder_name)
-    # Generar entre 1 y 10 archivos cada 2 minutos
+
     file_to_generate = random.randint(1, 10)
     for _ in range(file_to_generate):
         file_name = generate_file_names()
@@ -74,28 +82,34 @@ def generate():
             logging.warning("Error al crear el archivo {}: {}".format(file_name, e))
 
 
-
-# Se define una clase llamada apl_main, que encapsula el método principal del programa.
 class apl_main():
-    # El decorador @staticmethod indica que este método puede ser llamado en la clase sin crear una instancia de la misma.
+    """A class called apl_main is defined, which encapsulates the main method of the program.
+    The @staticmethod decorator indicates that this method can be called on the class without
+    creating an instance of it.
+    """
+
     @staticmethod
     def main():
+        """
+        Initiates the main program loop.
+        The main loop continuously calls the 'Synchronization' function to create the 'devices' folder
+        and generate files according to specified requirements. It then calls the 'generate' function
+        to create files within the 'devices' folder. After each iteration, the loop waits for 20 seconds
+        """
         Synchronization()
-        # Se inicia un bucle infinito que se ejecutará continuamente.
+        
         while True:
-            # Se llama a la función Synchronization(), que crea la carpeta "devices" y genera archivos según los requisitos.
             
             generate()
 
-            # Se añade un comentario para explicar que se espera 2 minutos antes de la próxima generación de archivos.
-            # Esto cumple con el requisito de generar archivos cada 2 minutos.
             time.sleep(20)
 
-# La siguiente línea se asegura de que el bloque de código debajo de ella solo se ejecute si este script es ejecutado directamente.
 if __name__ == "__main__":
-    # Configuración del sistema de registro (logging) para mostrar mensajes de nivel INFO o superior.
-    # Esto proporciona información sobre la ejecución del script, como mensajes de éxito o advertencias.
+    """
+    configures the logging system to display messages of INFO level or higher.
+    This provides information about the execution of the script, such as success messages or warnings.
+    The static main() method of the apl_main class is called to start the program execution.
+    """
     logging.basicConfig(level=logging.INFO)
 
-    # Se llama al método estático main() de la clase apl_main para iniciar la ejecución del programa.
     apl_main.main()
